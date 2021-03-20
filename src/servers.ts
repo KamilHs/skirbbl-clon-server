c923aadbba052f1edc2c8377234607a7053976dd
KamilHs <kamilsalimli1940@gmail.com>
2021-03-21 00:23:41 +0400

    feat: added server and its' organization

---
+++ src/servers.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import socketIO from "socket.io";
+
+export const app = express();
+export const io = new socketIO.Server();
+const server = require("http").createServer(app);
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(helmet());
+
+io.listen(server);
+app.listen(process.env.PORT || 5555);
---
+++ src/sockets/index.ts
@@ -0,0 +1,4 @@
+import { io } from "../servers";
+io.on("connection", (socket) => {});
+
+export default io;
