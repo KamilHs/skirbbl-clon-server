c450487e5d2883a815e210c21a2e5bbdf750c506
KamilHs <kamilsalimli1940@gmail.com>
2021-03-21 13:04:22 +0400

    feat: added get players data function

--- src/sockets/classes/Game.ts
+++ src/sockets/classes/Game.ts
@@ -1,10 +1,9 @@
-import Player from "./Player";
+import Player, { IPlayerData, PlayerType } from "./Player";
 
 export interface IGameConfig {
     rounds: number;
     roundTime: number;
 }
-
 class Game {
     private timeout: ReturnType<typeof setTimeout> | null = null;
     private currentPlayer: Player | null = null;
@@ -17,6 +16,16 @@ class Game {
         private id: string,
         private config: IGameConfig
     ) {}
+
+    getPlayersWithRoles(): IPlayerData[] {
+        return this.players.map((player) => ({
+            nickname: player.getNickname(),
+            role:
+                player.getSocket().id === this.creator.getSocket().id
+                    ? PlayerType.admin
+                    : PlayerType.member,
+        }));
+    }
 }
 
 export default Game;
--- src/sockets/classes/Player.ts
+++ src/sockets/classes/Player.ts
@@ -1,11 +1,29 @@
 import { Socket } from "socket.io";
 
+export enum PlayerType {
+    admin,
+    member,
+}
+
+export interface IPlayerData {
+    nickname: string;
+    role: PlayerType;
+}
+
 class Player {
     constructor(
         private socket: Socket,
         private nickname: string,
         private score: number
     ) {}
+
+    getNickname() {
+        return this.nickname;
+    }
+
+    getSocket() {
+        return this.socket;
+    }
 }
 
 export default Player;
